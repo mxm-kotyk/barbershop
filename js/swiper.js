d4ec035a97dfee54508c452ac1cb25765e048e32
Maksym Kotyk <mxm.kotyk@gmail.com>
2022-12-02 00:52:10 +0100

    Finished Slider

---
+++ js/swiper.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+  // If we need pagination
+pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + "</span>";
+          },
+        },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
